7f9099bcec82ce9a138f8a8033705d8a417ac723
lalithreddy <lalithreddy@192.168.0.251>
2019-05-10 13:17:12 -0400

    commit to branch 1

--- Product/src/main/java/com/mtc/app/product/Product.java
+++ Product/src/main/java/com/mtc/app/product/Product.java
@@ -8,4 +8,38 @@ public class Product {
 	private int price;
 	private int quantity;
 	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
 }
