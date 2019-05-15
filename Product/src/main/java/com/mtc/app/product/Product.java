468bc172471908fb0bd8678861273efdeb1ca0b8
lalithreddy <lalithreddy@192.168.0.251>
2019-05-15 13:23:55 -0400

    May15 12:23

--- Product/src/main/java/com/mtc/app/product/Product.java
+++ Product/src/main/java/com/mtc/app/product/Product.java
@@ -39,6 +39,64 @@ public class Product {
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + price;
+		result = prime * result + quantity;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price != other.price)
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", description=" + description + ", name=" + name + ", price=" + price
+				+ ", quantity=" + quantity + "]";
+	}
+	
+	public Product(int id, String description, String name, int price, int quantity) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product() {
+		super();
+	}
 	
 	
 	
